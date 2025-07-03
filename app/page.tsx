c61d10d525bd3978474bee9c477c5470d3c21a19
arisas <arisashiraishi@outlook.com>
2025-07-03 17:31:03 +0200

    mobile style tweak

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       backgroundImage="url('/images/hands/handsTwo.png')"
       pageTitle="/"
     >
-      <div className="space-y-12 items-center justify-center text-right mt-12 md:text-3xl lg:text-5xl max-w-5xl ml-auto">
+      <div className="space-y-12 items-center justify-center text-right mt-12 text-xl md:text-3xl lg:text-5xl max-w-5xl ml-auto">
         <p className="text-accent leading-relaxed italic">
           In humanity&apos;s last act, who plays god?
         </p>
